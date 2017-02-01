592949ece0925e6e9499304f75a311e0ed66fdf3
Jeff Fredericks <jeffwfredericks@gmail.com>
2017-01-31 19:53:59 -0700

    My results to the first assignment.

---
+++ index.js
@@ -0,0 +1,13 @@
+// Setup variables and constants
+var message = 'hello';
+let greeting = 'Yo!';
+const saying = 'What up?';
+
+// Reassign variables
+message = 'world';
+greeting = 'hello';
+
+// This line will throw an error, as a const is immutable
+saying = 'Booyah';
+
+
