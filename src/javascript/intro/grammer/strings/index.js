87abcbe9866abb5e68f7758088d0e734650f5675
Jeff Fredericks <jeffwfredericks@gmail.com>
2017-02-08 20:02:02 -0700

    Finished String Assignment

--- src/javascript/intro/grammer/strings/index.js
+++ src/javascript/intro/grammer/strings/index.js
@@ -1,3 +1,9 @@
 export const greeting = 'Hello World';
-export const characterAt = greeting.charAt(3);
-
+export const characterAt = greeting.charAt(4);
+export const greetingArray = greeting.split('');
+export const begins = (str, char) => {
+  return str[0] == char;
+};
+export const beginsInsensitive = (str, char) => {
+  return str[0].toLowerCase() == char.toLowerCase();
+};
