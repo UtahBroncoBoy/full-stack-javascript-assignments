96325021ee28dd00220a816d45ebff24231d832b
Jeff Fredericks <jeffwfredericks@gmail.com>
2017-02-15 19:17:29 -0700

    Finish the conditionals assignment.

--- src/javascript/intro/grammer/control-flow/conditionals/index.js
+++ src/javascript/intro/grammer/control-flow/conditionals/index.js
@@ -1,8 +1,11 @@
 export const greeting = (age = 0) => {
-
+  if(age > 18)  {
+    return 'Hello!';
+  }
+  return 'Yo!';
 };
 
-export const isTacoTuesday = (day = '') => {
-
+export const isTacoTuesday = (day) => {
+  return day === 'tuesday';
 };
 
